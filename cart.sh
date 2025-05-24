04a7bcce9cea31e5fc223d030244f9758b0f5775
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-25 01:42:35 +0530

    added content

--- cart.sh
+++ cart.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+
+#importing the function varabiles and all other stuff
+source ./common_script.sh
+
+#assiging server for installation
 service_name="cart"
+
 # Color codes
 color
+
 # Root privilege check
 check_root
+
 # logfile folder setup
 logfile_setup
+
 # Validation function
 VALIDATE
+
 # Node.js setup
 nodejs_setup
+
 # Application setup
 app_setup
+
 # Systemd Service setup
 systemd_setup
