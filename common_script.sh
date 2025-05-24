3873c388d4248606231d825c23a1e310e561c53d
vicky <vaikuntaraojagalingi@gmail.com>
2025-05-25 02:50:32 +0530

    added content

--- common_script.sh
+++ common_script.sh
@@ -97,4 +97,16 @@ VALIDATE $? "Installing Node.js dependencies"
 
 }
 
+# maven installation function 
+maven_setup(){
+    dnf install maven -y &>> "$LOG_FILE"
+    VALIDATE $? "installation of maven"
+    
+    mvn clean package &>> "$LOG_FILE"
+    VALIDATE $? "mvn package cleaning"
+
+    mv target/shipping-1.0.jar shipping.jar  &>> "$LOG_FILE"
+    VALIDATE $? "moving of shipping of jar"
+
+}
 
